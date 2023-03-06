274e647959b8aa742421d92bb95fd1406ff75481
Ricardo Polanco <polancoblancoricardo@gmail.com>
2023-03-06 09:32:47 -0600

    create a player :construction:

--- app.js
+++ app.js
@@ -6,23 +6,40 @@ canvas.height=innerHeight;
 
 class Player{
     constructor(){
-        this.position={
-            x:200,
-            y:200
-        }
         this.velocity={
             x:0,
             y:0
-        }
-        //this.image=
-        this.width=100
-        this.height=100
-    }
+        };
+        
+        const image = new Image();
+        image.src = './image/spaceship.png';
+        image.onload = () =>{
+            const scale = .15;
+            this.image= image;
+            this.width=image.width *scale;
+            this.height=image.height *scale;
+            this.position={
+                x:canvas.width / 2 -this.width /2,
+                y:canvas.height - this.height -40,
+            };
+        };
+
+    };
     draw(){
-        c.fillStyle='red'
-        c.fillRect(this.position.x,this.position.y,this.width,this.height)
-    }
-}
+        // c.fillStyle='red';
+        // c.fillRect(this.position.x,this.position.y,this.width,this.height);
+        if(this.image)
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    };
+};
 
 const player =new Player();
 player.draw();
+
+function animate(){
+    requestAnimationFrame(animate);
+    c.fillStyle='black';
+    c.fillRect(0,0, canvas.width,canvas.height);
+    player.draw();
+};
+animate();
